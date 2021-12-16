55d2e6434e5e30b0c552ea7bc0320a694638f93e
Harri <github@harritaito.com>
2021-12-16 12:14:08 +0200

    Router init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
     </div>
-    <h1>Harvest shopping list</h1>
     <router-view />
   </div>
 </template>
